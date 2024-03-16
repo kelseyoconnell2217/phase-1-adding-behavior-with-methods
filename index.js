58bcbcca6bdfc5bf0c678bdc29305f6f8d683f08
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2024-03-15 21:59:28 -0400

    Complete!

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+class Cat {
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex}
+    speak() {return `${this.name} says meow!`}
+        
+    
+  }
+  
+  class Dog {
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex
+        
+  }
+  speak() {return `${this.name} says woof!`}
+}
+const cat = new Cat("Sasha", "female")
+const dog = new Dog("Rufio", "male")
+class Bird {
+  constructor(name, sex){
+      this.name = name
+      this.sex = sex}
+  speak() {
+      if(this.sex === 'male'){
+          return  `It's me! ${this.name}, the parrot!`
+      } else {return `${this.name} says squawk!`}
+  }
+
+  }
+  const bird = new Bird("Pablo", "male")
+  const bird2 = new Bird("Mable", "female")
+
+cat.speak()
+dog.speak()
+bird.speak()
+bird2.speak()
